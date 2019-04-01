4d8f6359190abddfbaf7b0b6cf9cea602d3fcafa
sbx_user1059 <sbx_user1059@ip-10-128-0-30.eu-west-2.compute.internal>
2019-04-01 22:03:17 +0000

    Mon Apr 01 22:03:17 UTC 2019

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -13,3 +13,16 @@ class TestChk():
     def test_invalid_input(self):
         assert checkout_solution('1') == -1
         assert checkout_solution('E') == -1
+
+    def test_individual_sku(self):
+        assert checkout_solution('A') == 50
+        assert checkout_solution('B') == 30
+        assert checkout_solution('C') == 20
+        assert checkout_solution('D') == 15
+
+    def test_special_offers(self):
+        assert checkout_solution('AAA') == 130
+        assert checkout_solution('BB') == 45
+        assert checkout_solution('AAAA') == 180
+        assert checkout_solution('AAAAA') == 230
+        assert checkout_solution('AAAAAA') == 260
