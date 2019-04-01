f6524f0b7f7794420790aa15e823615120b04c8d
sbx_user1059 <sbx_user1059@ip-10-128-0-30.eu-west-2.compute.internal>
2019-04-01 21:44:29 +0000

    Mon Apr 01 21:44:29 UTC 2019

--- test/solution_tests/HLO/test_hlo.py
+++ test/solution_tests/HLO/test_hlo.py
@@ -3,4 +3,4 @@ from solutions.HLO import hello_solution
 
 class TestHlo():
     def test_hlo(self):
-        assert hello_solution.hello("Peter") == "Hello"
+        assert hello_solution.hello("Peter") == "Hello, World!"
